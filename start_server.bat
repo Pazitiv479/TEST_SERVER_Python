71996d855fa12c8311817b7920d6c6836b79ad7e
Pazitiv479 <43988115+Pazitiv479@users.noreply.github.com>
2026-08-27 16:54:47 +0700

    Update
    111

--- start_server.bat
+++ start_server.bat
@@ -2,23 +2,27 @@
 chcp 65001 >nul
 echo === Подготовка сервера к запуску ===
 
-:: 1. Проверяем, есть ли уже виртуальное окружение
-if not exist ".venv" (
+:: Заходим в папку, где лежит этот .bat файл
+cd /d "%~dp0"
+
+:: 1. Создаем виртуальное окружение, если его нет
+if not exist ".venv\Scripts\python.exe" (
     echo Создаю виртуальное окружение...
-    python -m venv .venv
+    py -3.14 -m venv .venv
 )
 
-:: 2. Активируем виртуальное окружение
-echo Активирую окружение...
-call .venv\Scripts\activate.bat
+:: 2. Обновляем установщик pip
+echo Обновляю PIP...
+.venv\Scripts\python.exe -m pip install --upgrade pip
 
-:: 3. Устанавливаем/обновляем зависимости
+:: 3. Устанавливаем библиотеки (теперь только websockets)
 echo Устанавливаю библиотеки...
-pip install -r requirements.txt
+.venv\Scripts\python.exe -m pip install -r requirements.txt
 
 :: 4. Запускаем сервер
-echo Запускаю сервер...
 echo.
-python TestServerPython.py
+echo Запускаю сервер...
+echo ====================================
+.venv\Scripts\python.exe TestServerPython.py
 
 pause
